c041c4384e402e2d6ef358c9d3160ba7d711bf14
bhumi11 <46476182+bhumi11@users.noreply.github.com>
2023-08-11 17:26:10 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,17 @@
+module.exports = {
+    users: [
+      {
+        id: 1,
+        name: "Helicopter",
+        price: 400,
+        imageUrl:
+          "",
+      },
+      {
+        id: 2,
+        name: "Army Tank",
+        price: 700,
+        imageUrl: "",
+      },
+    ],
+  };
